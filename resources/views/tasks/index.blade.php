ce30e4e70d3472a1e0d01d6928edf47734b90f01
shiho.niwa <s.t.luxaeterna@gmail.com>
2019-07-24 06:24:05 +0000

    validation

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク一覧</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+{!! link_to_route('messages.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
+
+@endsection
